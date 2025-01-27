ad92abce02c93c1e80ae6e99606ec5255c7f4e72
Shivam <shivamhonra@gmail.com>
2025-01-27 12:27:32 +0530

    Integrated share link and display content based on link

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,9 @@
+import { Dashboard } from "./pages/Dashboard"
+import { SharedBrain } from "./pages/SharedBrain"
 import { Signin } from "./pages/Signin"
 import { Signup } from "./pages/Signup"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import { Dashboard } from "./pages/dashboard"
+
 function App() {
   return <BrowserRouter>
     <Routes>
@@ -9,6 +11,7 @@ function App() {
       <Route path="/signup" element={<Signup />} />
       <Route path="/signin" element={<Signin />} />
       <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/dashboard/share/:hash" element={<SharedBrain />} />
     </Routes>
   </BrowserRouter>
 }
