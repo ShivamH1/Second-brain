6f47fcb5e2bb38b6661d934e116ce74fffd36f3d
ShivamH1 <shivamhonra@gmail.com>
2025-02-11 21:24:23 +0530

    Fixes before deployment

--- frontend/src/hooks/useContent.tsx
+++ frontend/src/hooks/useContent.tsx
@@ -3,29 +3,30 @@ import { useEffect, useState } from "react";
 import { BACKEND_URL } from "../config";
 
 export function useContent() {
-    const [content, setContents] = useState([]);    
+  const [content, setContents] = useState([]);
 
-    function refresh() {
-        axios.get(`${BACKEND_URL}/api/v1/content`, {
-            headers: {
-                "Authorization": localStorage.getItem("token")
-            }
-        })
-            .then((response) => {
-                setContents(response?.data?.contents)
-            })
-    }
+  function refresh() {
+    axios
+      .get(`${BACKEND_URL}/api/v1/content`, {
+        headers: {
+          Authorization: localStorage.getItem("token"),
+        },
+      })
+      .then((response) => {
+        setContents(response?.data?.contents || []);
+      });
+  }
 
-    useEffect(() => {
-        refresh()
-        const interval = setInterval(() => {
-            refresh()
-        }, 10 * 1000)
+  useEffect(() => {
+    refresh();
+    const interval = setInterval(() => {
+      refresh();
+    }, 10 * 1000);
 
-        return () => {
-            clearInterval(interval);
-        }
-    }, [])
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
 
-    return {content, refresh};
+  return { content, refresh };
 }
